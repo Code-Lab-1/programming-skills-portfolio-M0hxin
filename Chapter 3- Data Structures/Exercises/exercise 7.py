3764bf7c0151d10fb4b5edb4ee6e38b3f0f6355c
mohsin <mohhsinaliniamat1234@gmail.com>
2022-11-02 10:14:49 -0700

    new code ex 3.7

---
+++ Chapter 3- Data Structures/Exercises/exercise 7.py
@@ -0,0 +1,32 @@
+locations = ["Rome, Italy","Maui, Hawaii","New Zealand","Istanbul, Turkey","London, England"]
+
+print("Original order:")
+print(locations)
+
+print("Alphabetical:")
+print(sorted(locations))
+
+print("Original order:")
+print(locations)
+
+print("Reverse alphabetical:")
+print(sorted(locations, reverse=True))
+
+print("Original order:")
+print(locations)
+
+print("Reversed:")
+locations.reverse()
+print(locations)
+
+print("Original order:")
+locations.reverse()
+print(locations)
+
+print("Alphabetical")
+locations.sort()
+print(locations)
+
+print("Reverse alphabetical")
+locations.sort(reverse=True)
+print(locations)
